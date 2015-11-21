17d49e8b71f59ed3b75b6be3ceae51d92f6bbccb
dzhumhur <dzhumhur.n@gmail.com>
2015-11-21 12:48:09 +0200

    Added Exception Handling Excercise

---
+++ OOP C#/Exercise in Class/ExceptionHandling/ValidPerson/ConsoleClient.cs
@@ -0,0 +1,39 @@
+﻿namespace ValidPerson
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class ConsoleClient
+    {
+        static void Main(string[] args)
+        {
+            CheckIfPersonIsValid("Pesho", "Peshov", 24);
+            CheckIfPersonIsValid(string.Empty, "Goshev", 31);
+            CheckIfPersonIsValid("Manol", null, 63);
+            CheckIfPersonIsValid("Stoyan", "Kolev", -1);
+
+        }
+
+        public static void CheckIfPersonIsValid(string firstName, string lastName, int age)
+        {
+            try
+            {
+                var currPerson = new Person(firstName, lastName, age);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("Exception thrown: {0}", ex.Message);
+                Console.WriteLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception thrown: {0}", ex.Message);
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
---
+++ OOP C#/Exercise in Class/ExceptionHandling/ValidPerson/Person.cs
@@ -0,0 +1,64 @@
+﻿namespace ValidPerson
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        public string FirstName
+        {
+            get { return this.firstName; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception("Firstname cannot be null, empty or whitespaces");
+                }
+
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get { return this.lastName; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception("Lastname cannot be null, empty or whitespaces");
+                }
+
+                this.lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set
+            {
+                if (value < 0 || value > 120)
+                {
+                    throw new ArgumentOutOfRangeException("Age should be in range [0...120]");
+                }
+
+                this.age = value;
+            }
+        }
+    }
+}
