278da858a0ca6249364183e25bd0456406fc40c1
dzhumhur <dzhumhur.n@gmail.com>
2015-11-21 14:00:27 +0200

    Added Exception Handling Homework

---
+++ OOP C#/Homeworks/Exception Handling/01. Square Root/SquareRoot.cs
@@ -0,0 +1,33 @@
+﻿namespace _01.Square_Root
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class SquareRoot
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            try
+            {
+                uint number = uint.Parse(input);
+                Console.WriteLine(Math.Sqrt(number));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
---
+++ OOP C#/Homeworks/Exception Handling/02. Enter Numbers/EnterNumbers.cs
@@ -0,0 +1,48 @@
+﻿namespace _02.Enter_Numbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class EnterNumbers
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (!ReadNumber(1, 100))
+                {
+                    i--;
+                }  
+            }
+        }
+
+        static bool ReadNumber(int start, int end)
+        {
+            try
+            {
+                string input = Console.ReadLine();
+                var number = int.Parse(input);
+
+                if (number < start || number > end)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                return true;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("The number should be in range [{0}...{1}]", start.ToString(), end.ToString());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("The number should be positive integer number");
+            }
+
+            return false;
+        }
+    }
+}
