3be38065b900aca7ed1888c5ff16f79d08ee7d31
dzhumhur <dzhumhur.n@gmail.com>
2016-01-15 07:33:38 +0200

    Added High Quality Code Course Projects

---
+++ High Quality Code/Exercise In Class/High Quality Methods/Fibonacci Sequence With Loop/FibSeqLoop.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Sequence_With_Loop
+{
+    using System.Diagnostics;
+
+    class FibSeqLoop
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            FibonacciIterative(10);
+            sw.Stop();
+            var iterative = sw.Elapsed;
+
+            sw.Restart();
+            FibonacciRecursively(0, 1, 1, 10);
+            sw.Stop();
+            var recursive = sw.Elapsed;
+
+            Console.WriteLine("Iterative  = {0}", iterative);
+            Console.WriteLine("Recursive  = {0}", recursive);
+        }
+
+        static void FibonacciIterative(int len)
+        {
+            BigInteger firstNumber = 0;
+            BigInteger secondNumber = 1;
+            BigInteger sum = 0;
+
+            for (int i = 1; i < len; i++)
+            {
+                //Console.WriteLine(sum);
+                firstNumber = secondNumber;
+                secondNumber = sum;
+                sum = firstNumber + secondNumber;
+            }
+        }
+
+        static void FibonacciRecursively(BigInteger a, BigInteger b, int counter, int len)
+        {
+            //Console.WriteLine(a);
+            if (counter < len)
+            {
+                counter++;
+                FibonacciRecursively(b, a + b, counter, len);
+            }
+        }
+    }
+}
---
+++ High Quality Code/Exercise In Class/Naming Identifiers InCode/01. Writing New Code/ArraySlider.cs
@@ -0,0 +1,72 @@
+﻿namespace _01.Writing_New_Code
+{
+    using System;
+    using System.Linq;
+    using System.Numerics;
+
+    class ArraySlider
+    {
+        static void Main()
+        {
+            BigInteger[] numbers = Console.ReadLine()
+                                   .Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries)
+                                   .Select(BigInteger.Parse).ToArray();
+            var currLine = Console.ReadLine();
+            var currIndex = 0;
+            while (currLine != "stop")
+            {
+                var command = currLine.Split();
+                var offset = int.Parse(command[0]) % numbers.Length;
+                var operation = command[1];
+                var operand = int.Parse(command[2]);
+
+                if (offset < 0)
+                {
+                    offset += numbers.Length;
+                }
+
+                currIndex = (currIndex + offset) % numbers.Length;
+                var changedNumber = PerformOperation(operation, numbers[currIndex], operand);
+                numbers[currIndex] = changedNumber;
+                currLine = Console.ReadLine();
+            }
+
+            Console.Write("[");
+            Console.Write(string.Join(", ", numbers));
+            Console.Write("]");
+        }
+
+        static BigInteger PerformOperation(string operationType, BigInteger firstNumber, int secondNumber)
+        {
+            BigInteger result = 0;
+            switch (operationType)
+            {
+                case "&":
+                    result = firstNumber & secondNumber;
+                    break;
+                case "|":
+                    result = firstNumber | secondNumber;
+                    break;
+                case "+":
+                    result = firstNumber + secondNumber;
+                    break;
+                case "-":
+                    result = (firstNumber - secondNumber) < 0 ? 0 : (firstNumber - secondNumber);
+                    break;
+                case "*":
+                    result = firstNumber * secondNumber;
+                    break;
+                case "/":
+                    result = firstNumber / secondNumber;
+                    break;
+                case "^":
+                    result = (firstNumber ^ secondNumber) < 0 ? 0 : (firstNumber ^ secondNumber); ;
+                    break;
+                default:
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
