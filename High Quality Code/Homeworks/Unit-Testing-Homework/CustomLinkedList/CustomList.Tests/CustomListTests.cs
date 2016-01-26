9ad8b5448ff5ac40af6ef870f8d7e4f96c97e3b4
dzhumhur <dzhumhur.n@gmail.com>
2016-01-26 21:28:23 +0200

    Added Unit Testing Homework

---
+++ High Quality Code/Homeworks/Unit-Testing-Homework/CustomLinkedList/CustomList.Tests/CustomListTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CustomList.Tests
+{
+    using CustomLinkedList;
+
+    [TestClass]
+    public class CustomListTests
+    {
+        private DynamicList<int> linkedList;
+
+        [TestInitialize]
+        public void InitializeLinkedList()
+        {
+            this.linkedList = new DynamicList<int>();
+        }
+
+        [TestMethod]
+        public void AddMethod_ShouldAdd_TheValueInTheEndOF()
+        {
+            this.linkedList
+        }
+
+        [TestMethod]
+        public void Index_ShouldReturn_TheValueOfSearchedIndex()
+        {
+            this.linkedList.Add(4);
+            var returnedNumber = this.linkedList[0];
+            Assert.AreEqual(4, returnedNumber);
+        }
+
+       
+
+        [TestMethod]
+        public void removeat()
+        {
+
+        }
+
+        [TestMethod]
+        public void remove()
+        {
+
+        }
+
+        [TestMethod]
+        public void contains()
+        {
+
+        }
+    }
+}
